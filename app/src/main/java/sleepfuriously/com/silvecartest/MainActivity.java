e73239447cd588b3abd7ed84a958b7041cf27f12
sbiggs <scott.biggs@consensus-builder.com>
2019-08-09 12:20:59 -0500

    first commit

---
+++ app/src/main/java/sleepfuriously/com/silvecartest/MainActivity.java
@@ -0,0 +1,14 @@
+package sleepfuriously.com.silvecartest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
