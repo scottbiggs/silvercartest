b09fb067a8224b5d3310afb96432a9d6ef8699b3
sbiggs <scott.biggs@consensus-builder.com>
2019-08-11 12:35:13 -0500

    seems like two-pane mode is detected and is beginning to work

--- app/src/main/java/sleepfuriously/com/silvercartest/view/MainActivity.java
+++ app/src/main/java/sleepfuriously/com/silvercartest/view/MainActivity.java
@@ -104,6 +104,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+
 
     } // onCreate(savedInstanceState)
 
